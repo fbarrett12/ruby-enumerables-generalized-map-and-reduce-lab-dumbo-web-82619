94e8f93e9285b594924171d98c34a93e4f3d5d4e
Fredrick Barrett <f.barrett12@gmail.com>
2019-08-24 22:59:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+def map(arr)
+  new = []
+  for i in arr do
+    new.push(yield(i)) 
+  end
+  
+  return new
+end
+
+def reduce(arr, start = 0)
+  result = 0
+  for i in arr do
+    if i.is_a? Integer
+
+      puts yield(start, i)
+    else 
+      puts yield(start, i)
+    end
+  end
+  
+end
