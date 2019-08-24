7049de2a2c938f8dd65f4bb3f7235613e6f80347
Fredrick Barrett <f.barrett12@gmail.com>
2019-08-24 23:05:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,11 +9,15 @@ end
 
 def reduce(arr, acc = 0)
   
+  if arr.length == 0 
+    puts acc 
+  end
+  
   for i in arr do
     if i.is_a? Integer
-     puts reduce([], yield(start, i))
+      reduce([], yield(acc, i))
     else 
-      puts yield(start, i)
+      puts yield(acc, i)
     end
   end
   
